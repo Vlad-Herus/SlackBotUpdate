ad5e280efc70afca4e4d5cc438cd3bada84ee652
Vlad-Herus <VladimirGerusUA@gmail.com>
2021-10-04 23:59:55 +1100

    added missing cookie and response codes/triggers/responses

--- SlackBotUpdate/Program.cs
+++ SlackBotUpdate/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,6 +8,17 @@ namespace SlackBotUpdate
 {
     class Program
     {
+        const string FirstResponseCode = "Xr02D1V9D92R";
+        const string FirstResponseTrigger = "kibana is down";
+        const string FirstResponseResponse = "Isaac: is it {0}?";
+
+        const string SecondResponseCode = "Xr02CPH8L7D1";
+        const string SecondResponseTrigger = "kibana down";
+        const string SecondResponseResponse = "Isaac: is it {0}?";
+
+        const string Token = "";
+        const string Cookie = "";
+
         public class Response
         {
             public bool ok { get; set; }
@@ -17,19 +27,37 @@ namespace SlackBotUpdate
         static async Task Main(string[] args)
         {
             var dayOfWeek = GetDayOfTheWeek();
-            string myToken = "";
 
+            await UpdateResponse(
+                FirstResponseCode,
+                FirstResponseTrigger,
+                string.Format(FirstResponseResponse, dayOfWeek));
+
+            await UpdateResponse(
+                SecondResponseCode,
+                SecondResponseTrigger,
+                string.Format(SecondResponseResponse, dayOfWeek));
+        }
+
+        private static string GetDayOfTheWeek()
+        {
+            return DateTime.Now.DayOfWeek.ToString().ToLower();
+        }
+
+        private static async Task UpdateResponse(string responseCode, string responseTrigger, string responseResponse)
+        {
             try
             {
                 MultipartFormDataContent form = new MultipartFormDataContent();
-                form.Add(new StringContent("Xr02F6TFLX0F"), "response");
-                form.Add(new StringContent("vlad_test123"), "triggers");
-                form.Add(new StringContent("test 12 4 5 6 7 8 "), "responses");
-                form.Add(new StringContent(myToken), "token");
+                form.Add(new StringContent(responseCode), "response");
+                form.Add(new StringContent(responseTrigger), "triggers");
+                form.Add(new StringContent(responseResponse), "responses");
+                form.Add(new StringContent(Token), "token");
                 form.Add(new StringContent("edit_slackbot_response"), "_x_reason");
                 form.Add(new StringContent("online"), "_x_mode");
 
                 var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Cookie", Cookie);
                 var req = new HttpRequestMessage(HttpMethod.Post, "https://infotrack.slack.com/api/slackbot.responses.edit") { Content = form };
                 var res = await client.SendAsync(req);
 
@@ -44,16 +72,12 @@ namespace SlackBotUpdate
             }
             catch (Exception ex)
             {
-                var exFile = $@"D:\{dayOfWeek}.txt";
+                var exFile = $@"D:\{DateTime.Now.Ticks}.txt";
 
                 File.WriteAllText(exFile, ex.ToString());
                 System.Diagnostics.Process.Start("notepad.exe", exFile);
             }
         }
 
-        private static string GetDayOfTheWeek()
-        {
-            return DateTime.Now.DayOfWeek.ToString();
-        }
     }
 }
